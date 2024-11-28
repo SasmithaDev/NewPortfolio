7504395fa07b75c9d77656081eb0fdd7c144cde2
Sasmitha Weerakkody 🇪🇸 <151928231+SasmithaDev@users.noreply.github.com>
2024-11-28 09:34:27 +0000

    innitial commit 1

---
+++ app/routes/projects.smart-sparrow/smart-sparrow.jsx
@@ -0,0 +1,163 @@
+import backgroundSprLarge from '~/assets/spr-background-large.jpg';
+import backgroundSprPlaceholder from '~/assets/spr-background-placeholder.jpg';
+import backgroundSpr from '~/assets/spr-background.jpg';
+import imageSprComponentsDarkLarge from '~/assets/spr-components-dark-large.png';
+import imageSprComponentsDarkPlaceholder from '~/assets/spr-components-dark-placeholder.png';
+import imageSprComponentsDark from '~/assets/responsive.png';
+import imageSprComponentsLightLarge from '~/assets/spr-components-light-large.png';
+import imageSprComponentsLightPlaceholder from '~/assets/spr-components-light-placeholder.png';
+import imageSprComponentsLight from '~/assets/responsive.png';
+import imageSprLessonBuilderDarkLarge from '~/assets/spr-lesson-builder-dark-large.jpg';
+import imageSprLessonBuilderDarkPlaceholder from '~/assets/spr-lesson-builder-dark-placeholder.jpg';
+import imageSprLessonBuilderDark from '~/assets/spr-lesson-builder-dark.jpg';
+import imageSprLessonBuilderLightLarge from '~/assets/spr-lesson-builder-light-large.jpg';
+import imageSprLessonBuilderLightPlaceholder from '~/assets/spr-lesson-builder-light-placeholder.jpg';
+import imageSprLessonBuilderLight from '~/assets/spr-lesson-builder-light.jpg';
+import { Footer } from '~/components/footer';
+import { Image } from '~/components/image';
+import { SegmentedControl, SegmentedControlOption } from '~/components/segmented-control';
+import { ThemeProvider, useTheme } from '~/components/theme-provider';
+import sliceSlides from '~/assets/responsive.png';
+import sliceSlidesLarge from '~/assets/responsive.png';
+import {
+  ProjectBackground,
+  ProjectContainer,
+  ProjectHeader,
+  ProjectImage,
+  ProjectSection,
+  ProjectSectionContent,
+  ProjectSectionHeading,
+  ProjectSectionText,
+  ProjectTextRow,
+} from '~/layouts/project';
+import { baseMeta } from '~/utils/meta';
+import { lazy } from 'react';
+import { media } from '~/utils/style';
+
+const Earth = lazy(() => import('./earth').then(module => ({ default: module.Earth })));
+const EarthSection = lazy(() =>
+  import('./earth').then(module => ({ default: module.EarthSection }))
+);
+
+const title = 'Orgainland | Herbal Supplement';
+const description =
+  'I served as the developer and designer for Orgainland, driving a major evolution of the platform. We reimagined its core functionality, focusing on creating an intuitive and engaging experience for organic product enthusiasts.';
+const roles = [
+  'Art Direction',
+  'UX and UI Design',
+  'Front End Development',
+];
+
+export const meta = () => {
+  return baseMeta({ title, description, prefix: 'Projects' });
+};
+
+export const SmartSparrow = () => {
+  const { theme, toggleTheme } = useTheme();
+  const isDark = theme === 'dark';
+  const themes = ['dark', 'light'];
+
+  const handleThemeChange = index => {
+    toggleTheme(themes[index]);
+  };
+
+  return (
+    <>
+      <ProjectContainer>
+        <ProjectBackground
+          opacity={isDark ? 0.5 : 0.8}
+          src={backgroundSpr}
+          srcSet={`${backgroundSpr} 1080w, ${backgroundSprLarge} 2160w`}
+          placeholder={backgroundSprPlaceholder}
+        />
+        <ProjectHeader
+          title={title}
+          description={description}
+          url="https://www.orgainland.com/"
+          roles={roles}
+        />
+        <ProjectSection padding="top">
+          <ProjectSectionContent>
+            <ProjectImage
+              raised
+              key={theme}
+              srcSet={
+                isDark
+                  ? `${imageSprLessonBuilderDark} 1280w, ${imageSprLessonBuilderDarkLarge} 2560w`
+                  : `${imageSprLessonBuilderLight} 1280w, ${imageSprLessonBuilderLightLarge} 2560w`
+              }
+              width={640}
+              height={400}
+              placeholder={
+                isDark
+                  ? imageSprLessonBuilderDarkPlaceholder
+                  : imageSprLessonBuilderLightPlaceholder
+              }
+              sizes={`(max-width: ${media.mobile}px) 100vw, (max-width: ${media.tablet}px) 800px, 1000px`}
+              alt="The aero lesson builder app dragging an audio component into a screen about plant cells."
+            />
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ProjectSection>
+          <ProjectTextRow>
+            <ProjectSectionHeading>What is Orgainland ?</ProjectSectionHeading>
+            <ProjectSectionText>
+            <li>Orgainland is a groundbreaking herbal supplement brand born from years of dedicated research and innovation. Our mission is to redefine wellness by offering 100% plant-based products that are rich in every essential nutrient your body needs. At the core of Orgainland is a commitment to promoting health and vitality through natural, sustainable solutions.</li>
+
+            <li>Our supplements are crafted with the finest organic ingredients, ensuring purity, quality, and effectiveness in every product. We aim to inspire healthier lifestyles while supporting the environment through our organic agriculture initiatives. By prioritizing sustainability, we strive to make a meaningful impact on both personal health and the planet.</li>
+
+            <li>Orgainland envisions becoming a global leader in the health and wellness industry, recognized for its dedication to crafting high-quality, plant-based products. Whether you're looking to enhance your nutrition or support your overall well-being, Orgainland is your trusted partner in achieving a healthier, more balanced life.</li>
+            </ProjectSectionText>
+          </ProjectTextRow>
+        </ProjectSection>
+        <ProjectSection light={isDark}>
+          <ProjectSectionContent>
+            <Image
+              key={theme}
+              srcSet={
+                isDark
+                  ? `${imageSprComponentsDark} 1024w, ${imageSprComponentsDarkLarge} 2048w`
+                  : `${imageSprComponentsLight} 1024w, ${imageSprComponentsLightLarge} 2048w`
+              }
+              width={1024}
+              hright={800}
+              placeholder={
+                isDark
+                  ? imageSprComponentsDarkPlaceholder
+                  : imageSprComponentsLightPlaceholder
+              }
+              alt={`A set of ${theme} themed components for the aero design system`}
+              sizes="100vw"
+            />
+            <ProjectTextRow>
+              <SegmentedControl
+                currentIndex={themes.indexOf(theme)}
+                onChange={handleThemeChange}
+              >
+                <SegmentedControlOption>Dark theme</SegmentedControlOption>
+                <SegmentedControlOption>Light theme</SegmentedControlOption>
+              </SegmentedControl>
+            </ProjectTextRow>
+            <ProjectTextRow>
+              <ProjectSectionHeading>Responsive Design</ProjectSectionHeading>
+              <ProjectSectionText>
+              Orgainland's website features a sleek, fully responsive design, ensuring a seamless user experience across all devices. With intuitive navigation and optimized layouts, the site adapts effortlessly to desktops, tablets, and smartphones. Its modern design highlights products and content beautifully, making it easy for users to explore and shop anywhere, anytime.
+              </ProjectSectionText>
+            </ProjectTextRow>
+            <Image
+              srcSet={`${sliceSlides} 800w, ${sliceSlidesLarge} 1920w`}
+              width={800}
+              height={500}
+              alt="The new My Slides tab in slice, showing annotated and favorited slides."
+              sizes={`(max-width: ${media.mobile}px) 500px, (max-width: ${media.tablet}px) 800px, 1000px`}
+            />
+          </ProjectSectionContent>
+        </ProjectSection>
+        <ThemeProvider theme="dark" data-invert>
+        </ThemeProvider>
+        
+      </ProjectContainer>
+      <Footer />
+    </>
+  );
+};
