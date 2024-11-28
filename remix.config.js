cd58f25bdd9bdaf825197208e366416195b67919
Sasmitha Weerakkody 🇪🇸 <151928231+SasmithaDev@users.noreply.github.com>
2024-11-28 17:21:36 +0000

    commit 3

--- remix.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-    appDirectory: "app",
-    assetsBuildDirectory: "public/build", // Where Remix builds static assets
-    publicPath: "/build/", // Path in the browser where assets are served
-    serverBuildPath: "build/index.js", // Server build output
-    ignoredRouteFiles: ["**/.*"], // Ignore hidden files
-  };
-  
